af495d5bb2558fd1bc10a152b9b76cd37c6f5888
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-20 06:48:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   return (delete Object.assign({}, object)[key])
+  //first you must make a clone of object using object assign with original
+  //object into an empty one. Then you must delete a key from the clone
+  //by doing delete object[key]. You then return that new object.
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
 }
