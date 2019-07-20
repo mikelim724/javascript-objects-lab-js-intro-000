229e2a62b6d95f58da83db965416b6f649949001
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-20 06:31:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object
+
 }
 
-function destructivelyUpdateObjectWithKeyAndValue {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
 }
